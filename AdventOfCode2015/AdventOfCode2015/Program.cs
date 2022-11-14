93ae01a926fb2b17525cd9a85938106f991ac028
Ronald <ronald.steinauer@gmail.com>
2022-11-14 19:59:54 +0100

    Base + Day 1

---
+++ AdventOfCode2015/AdventOfCode2015/Program.cs
@@ -0,0 +1,12 @@
+﻿using AdventOfCode2015.Days;
+
+internal class Program
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        Day01 Day = new Day01();
+
+        Console.ReadLine();
+    }
+}
