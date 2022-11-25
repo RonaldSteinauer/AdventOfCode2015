b1cb93240ee1c3f7bc030dfba72f80882b40ecb3
Ronald <ronald.steinauer@gmail.com>
2022-11-25 23:03:18 +0100

    Day 8 Stage 1

---
+++ AdventOfCode2015/Days/Day08.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace AdventOfCode2015.Days
+{
+    public class Day08
+    {
+        private readonly string[] input;
+
+        public Day08()
+        {
+            input = File.ReadAllLines(@"../../../Input/Day08.txt");
+
+            var result = Stage1();
+
+            Console.WriteLine(result);
+
+            Clipboard.SetText(result);
+        }
+
+        private string Stage1()
+        {
+            var values = input.Select(d => (d, Regex.Unescape(d), d.Count(), Regex.Unescape(d).Count() - 2));
+
+            return (values.Sum(d => d.Item3) - values.Sum(d => d.Item4)).ToString();
+        }
+
+        private string Stage2()
+        {
+            return "";
+        }
+    }
+}
--- AdventOfCode2015/Program.cs
+++ AdventOfCode2015/Program.cs
@@ -5,7 +5,7 @@ internal class Program
     [STAThread]
     private static void Main(string[] args)
     {
-        Day07 day = new Day07();
+        Day08 day = new Day08();
 
         Console.ReadLine();
     }
